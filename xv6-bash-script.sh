b1776aaf285f63da84e8888249fb77cdae444ecd
salmanzaffar <66812577+salmanzaffar@users.noreply.github.com>
2023-05-04 11:41:39 +0500

    Update xv6-bash-script.sh

--- xv6-bash-script.sh
+++ xv6-bash-script.sh
@@ -1,20 +1,5 @@
 ls
 
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-
-main(void){
-int pid = fork();
-if(pid > 0){
-printf("parent: child=%d\n", pid);
-pid = wait((int *) 0);
-printf("child %d is done\n", pid);
-} else if(pid == 0){
-printf("child: exiting\n");
-exit(0);
-} else {
-printf("fork error\n");
-}
-}
+pid=fork();
+printf("%d\n",pid);
 ls ../
