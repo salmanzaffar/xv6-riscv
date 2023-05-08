0bfe8579a641006d39b5a4c8479d9e02f87e1ada
salmanzaffar <66812577+salmanzaffar@users.noreply.github.com>
2023-05-08 12:34:06 +0500

    Update first.c

--- user/first.c
+++ user/first.c
@@ -1,14 +1,6 @@
-//A Simple C program
-#include "types.h"
-#include "stat.h"
-#include "user.h"
-	
-//passing command line arguments
-	
-int main(int argc, char *argv[])
-{
-printf(1, "My first xv6 program learnt at GFG\n");
-exit();
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
 }
-
-// This code is contributed by sambhav228
