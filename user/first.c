52255725d5158cd7d4bb4e3ef259b64cd5bd515f
salmanzaffar <66812577+salmanzaffar@users.noreply.github.com>
2023-08-08 09:37:08 -0700

    Update first.c

--- user/first.c
+++ user/first.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "common.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2) {
+	fprintf(stderr, "usage: cpu <string>\n");
+	exit(1);
+    }
+    char *str = argv[1];
+
+    while (1) {
+	printf("%s\n", str);
+	Spin(1);
+    }
+    return 0;
+}
+
+/*#include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
    printf("Hello, World!");
    return 0;
-}
+}*/
+
+
