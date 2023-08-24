74441d376d41778bec110e5698c5d48dde2e90f5
salmanzaffar <66812577+salmanzaffar@users.noreply.github.com>
2023-08-24 09:38:32 +0500

    Update first.c

--- user/first.c
+++ user/first.c
@@ -1,8 +1,20 @@
-#include <stdio.h>
-int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
+#include "kernel/types.h"
+#include "user/user.h"
+
+int main(void){
+int pid=fork();
+   if (pid>0){
+   //printf("parent: child=%d\n",pid);
+   pid=wait((int *)0);
+   printf("parent: child=%d\n",pid);
+   printf("child %d is done\n",pid);
+   }
+   else if(pid==0){
+   printf("child:exiting\n"); 
+   exit(0);
+   }
+   else{
+   printf("fork error\n");
+   }
    return 0;
-}
-
-
+   }
