a0beaa9c8f3216a7890925d2079a987d30425e22
salmanzaffar <66812577+salmanzaffar@users.noreply.github.com>
2023-08-26 22:14:04 +0500

    Update first.c

--- user/first.c
+++ user/first.c
@@ -1,8 +1,8 @@
-//#include "types.h"
-//#include "user.h"
+#include "types.h"
+#include "user.h"
 
-#include <stdio.h>
-#include <stdlib.h>
+//#include <stdio.h>
+//#include <stdlib.h>
 
 int main(void){
 int pid=fork();
